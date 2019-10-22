c7a47e0c8e648b55229ee676b65932086486738c
BugliL <bugli.lorenzo@gmail.com>
2019-10-22 15:15:02 +0200

    Refactoring
    Refactoring to clarify usage

--- python/django/force_login/middlewares.py
+++ python/django/force_login/middlewares.py
@@ -7,11 +7,14 @@ class AuthRequiredMiddleware(object):
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self, request : WSGIRequest):
+    def __call__(self, request: WSGIRequest):
         LOGIN_URL = reverse('users:login')
         response = self.get_response(request)
 
-        if not request.user.is_authenticated and request.path != LOGIN_URL:
+        is_authenticated = bool(request.user) and bool(request.user.is_authenticated)
+        is_not_login_url = request.path != LOGIN_URL
+
+        if (not is_authenticated) and is_not_login_url:
             return HttpResponseRedirect(LOGIN_URL)
 
         return response
